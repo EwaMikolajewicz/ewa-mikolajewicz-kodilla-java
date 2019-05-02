40b93eec451aaf5a6414f1267c547daa56e3eb91
EwaMikolajewicz <mikolajewicz.e@gmail.com>
2019-05-02 11:01:47 +0200

    Task_1402_poprawka_testCalc

--- kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
@@ -3,6 +3,9 @@ package com.kodilla.spring.calculator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class Calculator {
 
@@ -36,5 +39,17 @@ public class Calculator {
         display.displayValue(a / b);
         return a / b;
     }
+
+    public List<Double>  operation(double a, double b) {
+        List<Double> theList = new ArrayList<>();
+
+        theList.add(add(a, b));
+        theList.add(sub(a, b));
+        theList.add(mul(a, b));
+        theList.add(div(a, b));
+
+        return theList;
+    }
 }
+
 
--- kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -1,5 +1,6 @@
 package com.kodilla.spring.calculator;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,50 +8,86 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CalculatorTestSuite {
     private static int testCounter = 0;
 
-    @Test
-    public void testCalculations() {
-        //Given
-        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.calculator");
-        Calculator result = context.getBean(Calculator.class);
-
-        //When
-        result.add(3.5, 18.3);
-        result.sub(3.5, 18.3);
-        result.mul(3.5, 18.3);
-        result.div(3.5, 18.3);
-
-        //Then
-        //do nothing;
-    }
-
     @Before
     public void beforeTest(){
         testCounter++;
         System.out.println("\nTest nr: " + testCounter);
     }
+
     @Test
-    public void testDivZero() {
+    public void addTest() {
         //Given
         ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.calculator");
         Calculator result = context.getBean(Calculator.class);
 
         //When
-        try {
-            double value = result.div(3.5, 0);
-            System.out.println(value);
-
-        } catch (ArithmeticException e) {
-
-            System.out.println(e + ". Error!!! Choose divider <> 0 !!!");
-        }
+        double resultAdd = result.add(3.5, 18.3);
 
         //Then
-        //do nothing
+        Assert.assertEquals(21.8, resultAdd,0);
     }
+
+    @Test
+    public void subTest() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.calculator");
+        Calculator result = context.getBean(Calculator.class);
+
+        //When
+        double resultSub = result.sub(3.5, 18.3);
+
+        //Then
+        Assert.assertEquals(-14.8, resultSub,0);
+    }
+
+    @Test
+    public void mulTest() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.calculator");
+        Calculator result = context.getBean(Calculator.class);
+
+        //When
+        double resultMul = result.mul(3.5, 18.3);
+
+        //Then
+        Assert.assertEquals(64.05, resultMul,0);
+    }
+
+    @Test
+    public void divTest() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.calculator");
+        Calculator result = context.getBean(Calculator.class);
+
+        //When
+        double resultDiv = result.div(3.5, 18.3);
+
+        //Then
+        Assert.assertEquals(0.1912568306010929, resultDiv,0);
+    }
+
+    @Test
+    public void testCalc(){
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.calculator");
+        Calculator result = context.getBean(Calculator.class);
+
+        //When
+       List<Double> listOfResult = result.operation(3.5, 18.3);
+
+        //Then
+        Assert.assertTrue(listOfResult.get(0)==21.8);
+        Assert.assertTrue(listOfResult.get(1)==-14.8);
+        Assert.assertTrue(listOfResult.get(2)==64.05);
+        Assert.assertTrue(listOfResult.get(3)==0.1912568306010929);
+
+    }
+
 }
