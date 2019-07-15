946453ef25f50f4b22fcce88e4ee92d88b5e0722
EwaMikolajewicz <mikolajewicz.e@gmail.com>
2019-07-15 08:21:30 +0200

    zadanie 7.4

---
+++ src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -0,0 +1,24 @@
+package com.kodilla.stream.array;
+
+import java.util.stream.IntStream;
+
+public interface ArrayOperations {
+
+    static double getAverage(int[] numbers) {
+        if (numbers.length == 0) {
+            System.out.println("Array is empty.");
+            return 0;
+        } else {
+            System.out.println("Average of numbers: ");
+            IntStream.range(0, numbers.length)
+                    .map(n -> numbers[n])
+                    .forEach(System.out::println);
+
+            double average = IntStream.range(0, numbers.length)
+                    .map(n -> numbers[n])
+                    .average().getAsDouble();
+            return average;
+
+        }
+    }
+}
---
+++ src/test/java/com/kodilla/stream/array/ArrayOperationTestSuite.java
@@ -0,0 +1,63 @@
+package com.kodilla.stream.array;
+
+import org.junit.*;
+
+public class ArrayOperationTestSuite {
+    private static int testCounterS = 0;
+
+    @BeforeClass
+    public static void beforeAllTests() {
+        System.out.println("This is the beginning of tests.\n");
+    }
+
+    @AfterClass
+    public static void afterAllTests() {
+        System.out.println("\nAll tests are finished.");
+    }
+
+    @Before
+    public void beforeEveryTest() {
+        testCounterS++;
+        System.out.println("Preparing to execute test #" + testCounterS + "...");
+    }
+
+    @Test
+    public void testGetAverageWith0Numbers(){
+        //Given
+        int [] numbers = new int[0];
+
+        //When
+        double result = ArrayOperations.getAverage(numbers);
+
+        //Then
+        Assert.assertEquals(0, result,0.001);
+        Assert.assertEquals(0, numbers.length);
+    }
+
+    @Test
+    public void testGetAverageWith0(){
+        //Given
+        int [] numbers = new int[]{5,6,7,8,9,0};
+
+        //When
+        double result = ArrayOperations.getAverage(numbers);
+
+        //Then
+        Assert.assertEquals(5.8333, result,0.0001);
+        Assert.assertEquals(6, numbers.length);
+        Assert.assertEquals(7,numbers[2]);
+    }
+    @Test
+    public void testGetAverage(){
+        //Given
+        int [] numbers = new int[]{5,6,7,8,9};
+
+        //When
+        double result = ArrayOperations.getAverage(numbers);
+
+        //Then
+        Assert.assertEquals(7.0, result,0.001);
+        Assert.assertEquals(5, numbers.length);
+        Assert.assertEquals(9,numbers[4]);
+    }
+}
